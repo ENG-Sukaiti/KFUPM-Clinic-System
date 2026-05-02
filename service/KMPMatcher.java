eaec3d4a8aa90eee8d8343f2f38ef5e3f2f95502
Ibrahim Alsukaiti <ibrahim@Ibrahims-MacBook-Pro.local>
2026-05-02 22:35:17 +0300

    Haitham: did kmp

--- service/KMPMatcher.java
+++ service/KMPMatcher.java
@@ -1,11 +1,62 @@
 package kfupm.clinic.service;
 
 public class KMPMatcher implements StringMatcher {
+    
     @Override
     public boolean contains(String text, String pattern) {
-        // TODO: implement KMP substring search
-        throw new UnsupportedOperationException("TODO: KMPMatcher.contains");
+        if (pattern == null || pattern.isEmpty()) return true;
+        if (text == null) return false;
+
+        String t = text.toLowerCase();
+        String p = pattern.toLowerCase();
+
+        int n = t.length();
+        int m = p.length();
+        int[] lps = computeLPSArray(p);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < n) {
+            if (p.charAt(j) == t.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == m) {
+                return true;
+            } else if (i < n && p.charAt(j) != t.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
     }
 
-    // TODO: implement prefix function / LPS array helper
+    private int[] computeLPSArray(String pattern) {
+        int m = pattern.length();
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < m) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}r
 }
