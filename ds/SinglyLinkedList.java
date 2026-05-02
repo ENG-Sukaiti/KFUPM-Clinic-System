1f50322d2d82c76d8e9818aeef149c34d951124b
Ibrahim Alsukaiti <ibrahim@Ibrahims-MacBook-Pro.local>
2026-05-02 23:09:59 +0300

    final commition after testing everything works perfectly

--- ds/SinglyLinkedList.java
+++ ds/SinglyLinkedList.java
@@ -4,66 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** Students implement. */
+
 public class SinglyLinkedList<T> {
 
-    private class Node<T> {
+    private static class Node<T> {
         private T data;
         private Node<T> next; 
 
-        Node(){
-            this(null, null);
-        }
-
-        Node(T data){
-            this(data, null);
-        }
-
-        Node(T data, Node<T> node){
+        Node(T data) {
             this.data = data;
-            this.next = node;
+            this.next = null;
         }
     }
 
     private Node<T> head, tail;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
         head = tail = null;
     }
 
-    boolean isEmpty() {
-        return head == tail;
-    }
-
-    public void addToHead(T data){
-        Node<T> newNode = new Node<T>(data);
-
-        if (isEmpty()){
-            head = tail = newNode;
-            return;
-        }
-
-        newNode.next = head;
-        head = newNode;
+    public boolean isEmpty() {
+        return head == null;
     }
 
     public void addLast(T data) {
-        Node<T> newNode = new Node<T>(data);
-        if (isEmpty()){
+        Node<T> newNode = new Node<>(data);
+        if (isEmpty()) {
             head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
-
-        tail.next = newNode;
-        tail = newNode;
     }
+
     public List<T> toList() {
         List<T> list = new ArrayList<>();
-
         Node<T> ptr = head;
-
-        while (ptr.next != null){
+        while (ptr != null) {
             list.add(ptr.data);
+            ptr = ptr.next; 
         }
-
         return list;
     }
-}
+} 
